17d851ed80a18a7c09f45255155c160da186fc44
djangofire <djangopycon@gmail.com>
2016-10-01 23:05:12 -0400

    added runtime.txt

--- code_cafe/urls.py
+++ code_cafe/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='base.html')),
 ]
-
-urlpatterns += ['',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    ]
